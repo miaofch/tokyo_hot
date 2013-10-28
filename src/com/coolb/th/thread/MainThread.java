0902bb41d43d0c76e2e18df6742f170f4da46583
fucheng.miao <fucheng.miao@qunar.com>
2013-10-28 20:38:35 +0800

    config log4j

--- src/com/coolb/th/thread/MainThread.java
+++ src/com/coolb/th/thread/MainThread.java
@@ -1,9 +1,17 @@
 package com.coolb.th.thread;
 
+import org.apache.log4j.Logger;
+
 public class MainThread {
+	
+	private static final Logger log = Logger.getLogger(MainThread.class); 
 
 	public static void main(String[] args) {
-		
+		try {
+			Class.forName("com.coolb.th.config.THConfig");
+		} catch (ClassNotFoundException e) {
+			log.error("加载配置失败", e);
+		}
 	}
 	
 }
